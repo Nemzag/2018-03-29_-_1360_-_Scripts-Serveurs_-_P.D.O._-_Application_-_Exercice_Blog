cba77c0d087f713622c01fa3f95caa5008349004
Gaz‑mên Arifi <nemzag@hotmail.com>
2019-11-06 22:14:06 +0100

    Premier commit de archivage, registrage & sauvegarde historique de l'exercice de création de un blog basique, pour le cours de Script’s‑Serveur’s (P.H.P.)…

---
+++ inc/footer.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nemzag
+ * Date: 2018-02-23
+ * Time: 15:57
+ */
+?>
+</main>
+<footer>
+    <p style="text-align: center;">Web Developer : Arifi Gazmen&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Copyright : Arifi Gazmen&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Date : <?= $todayDate ?></p>
+</footer>
+</body>
+</html>
---
+++ inc/header.inc.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<!--
+/**
+ * Created by PhpStorm.
+ * User: nemzag
+ * Date: 2018-03-29
+ * Time: 14:35
+ * Updated: 2018-04-11 - 21:12
+ */
+ -->
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="author" content="Gazmen Arifi / Газмен Арифи">
+    <meta name="date" content="<?= $todayDate.' '.$todayHour; ?> UTC+1" scheme="YYYY-MM-DD h:m">
+    <title>PHP : <?= /*'PHP: '.*/$title ?></title>
+    <!--<title><?php /*echo $title; */?></title>-->
+    <link rel="stylesheet" href="<?= $pathProject ?>css/style.css">
+    <link rel="stylesheet" href="<?= $css ?>">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>PHP : <?= /*'PHP: '.*/$title ?></h1>
+</header>
+<main>
